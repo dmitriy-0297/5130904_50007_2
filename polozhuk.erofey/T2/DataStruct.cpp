d945413260ad2587266f3b32575c2844b0034f17
pouya350 <pravyparen1@mail.ru>
2026-04-02 23:02:38 +0300

    cpp file iofmtguard add

--- polozhuk.erofey/T2/DataStruct.cpp
+++ polozhuk.erofey/T2/DataStruct.cpp
@@ -133,3 +133,11 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& src){
     out << "key3 \"" << src.key3 << "\":)";
     return out;
 }
+iofmtguard::iofmtguard(std::basic_ios<char>& s): s_(s), width_(s.width()), fill_(s.fill()),
+precision_(s.precision()), fmt_(s.flags()){}
+iofmtguard::~iofmtguard(){
+    s_.width(width_);
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+}
