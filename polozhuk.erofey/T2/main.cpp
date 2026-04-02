ca3dea5dda39165803591f73d29d139b6ac474be
pouya350 <pravyparen1@mail.ru>
2026-04-02 23:22:33 +0300

    main tests  added

--- polozhuk.erofey/T2/main.cpp
+++ polozhuk.erofey/T2/main.cpp
@@ -1,5 +1,26 @@
-
-int main() {
+#include "DataStruct.h"
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+int main(){
+    std::vector<DataStruct> data;
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
+    std::sort(data.begin(), data.end(),
+        [](const DataStruct& a, const DataStruct& b){
+            if (a.key1 != b.key1) return a.key1 < b.key1;
+            if (a.key2 != b.key2) return a.key2 < b.key2;
+            return a.key3.size() < b.key3.size();
+        }
+    );
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
     return 0;
 }
-
