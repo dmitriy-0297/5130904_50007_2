801ad119fa8e6cdd7b5b49ae293ea1d7f05deae1
saber2244 <saber2244@debian.home>
2026-04-06 02:38:21 +0800

    fix bug4

--- zhu.zhanyu/T2/DataStruct.cpp
+++ zhu.zhanyu/T2/DataStruct.cpp
@@ -251,15 +251,30 @@ bool compareDataStruct(const DataStruct& a, const DataStruct& b)
 int main()
 {
     std::vector<DataStruct> data;
+    std::string line;
 
-    std::copy(std::istream_iterator<DataStruct>(std::cin),
-              std::istream_iterator<DataStruct>(),
-              std::back_inserter(data));
+    while (std::getline(std::cin, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+
+        std::istringstream iss(line);
+        DataStruct temp;
+
+        if (iss >> temp)
+        {
+            data.push_back(temp);
+        }
+    }
 
     std::sort(data.begin(), data.end(), compareDataStruct);
 
-    std::copy(data.begin(), data.end(),
-              std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    for (const auto& item : data)
+    {
+        std::cout << item << "\n";
+    }
 
     return 0;
 }
