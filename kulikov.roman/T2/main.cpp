760f12e091707eb8871fbf17c5fb282085cfa397
Roman Kulikov <kulikovrma@yandex.ru>
2026-04-06 21:40:18 +0300

    fix line endings

---
+++ kulikov.roman/T2/main.cpp
@@ -0,0 +1,36 @@
+#include "DataStruct.h"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main()
+{
+  std::vector< mashkin::DataStruct > data;
+
+  while (!std::cin.eof())
+  {
+    mashkin::DataStruct tmp{};
+    if (std::cin >> tmp)
+    {
+      data.push_back(tmp);
+    }
+    else
+    {
+      std::cin.clear();
+      std::string skip;
+      std::getline(std::cin, skip);
+    }
+  }
+
+  std::sort(data.begin(), data.end(), mashkin::DataStructComparator{});
+
+  std::copy(
+    data.cbegin(),
+    data.cend(),
+    std::ostream_iterator< mashkin::DataStruct >(std::cout, "\n")
+  );
+
+  return 0;
+}
