c0f5195bc27f9c58c3179766cc03875305277596
pouya350 <pravyparen1@mail.ru>
2026-04-28 00:58:16 +0300

    Point operator >> add

--- polozhuk.erofey/T3/main.cpp
+++ polozhuk.erofey/T3/main.cpp
@@ -9,6 +9,31 @@ namespace polozhuk{
     struct Polygon {
         std::vector<Point> points_;
     };
+    std::istream& operator>>(std::istream& in, Point& p) {
+        char c;
+        if (!(in >> c) || c != '(') {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        if (!(in >> p.x_)) {
+            return in;
+        }
+
+        if (!(in >> c) || c != ';') {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+        if ( !(in >> p.y_)) {
+            return in;
+        }
+        if ( !(in >> c) || c != ')') {
+            in.setstate(std::ios::failbit);
+            return in;
+        }
+
+        return in;
+    }
 
 
 
