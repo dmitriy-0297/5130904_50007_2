b0013df47e2d7f6c1d5d5dfe275dffeb8408b989
pouya350 <pravyparen1@mail.ru>
2026-04-28 01:29:59 +0300

    overr no for ech look>> Poligon

--- polozhuk.erofey/T3/main.cpp
+++ polozhuk.erofey/T3/main.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <iterator>
+
 namespace polozhuk{
     struct Point {
         int x_;
@@ -10,6 +13,7 @@ namespace polozhuk{
     struct Polygon {
         std::vector<Point> points_;
     };
+
     std::istream& operator>>(std::istream& in, Point& p) {
         char c;
         if (!(in >> c) || c != '(') {
@@ -35,22 +39,25 @@ namespace polozhuk{
 
         return in;
     }
-    std::istream& operator>>(std::istream& in, Polygon& poly) {
-        int n = 0;
-        if (!(in >> n)) {
+
+
+    std::istream& operator>>(std::istream& in, Polygon& p) {
+        std::istream::sentry sentry(in);
+        if (!sentry) {
             return in;
         }
-        if (n < 3) {
+        size_t countPoints = 0;
+        if (!(in >> countPoints) || countPoints < 3) {
             in.setstate(std::ios::failbit);
             return in;
         }
-        std::vector<Point> temp_points(n);
-        for (size_t i = 0; i < n; ++i) {
-            if (!(in >> temp_points[i])) {
-                return in;
-            }
+        std::vector<Point> tmp;
+        std::copy_n((std::istream_iterator<Point>{in}), countPoints, std::back_inserter(tmp));
+
+        if (in) {
+            p.points_ = std::move(tmp);
         }
-        poly.points_ = std::move(temp_points);
+
         return in;
     }
 
@@ -151,5 +158,28 @@ namespace polozhuk{
 
 
 int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Error: No input file provided.\n";
+        return 1;
+    }
+    std::string filename = argv[1];
+    std::ifstream inputFile(filename);
 
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Could not open file " << filename << "\n";
+        return 1;
+    }
+
+    std::vector<polozhuk::Polygon> polygons;
+    polozhuk::Polygon tempPoly;
+    while (inputFile >> tempPoly) {
+        polygons.push_back(tempPoly);
+    }
+    std::cout << "Successfully read " << polygons.size() << " polygons.\n";
+    return 0;
 }
+
+
+
+
+
