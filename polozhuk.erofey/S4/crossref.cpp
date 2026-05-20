8b24e1d217244ed081a79863cea03239967d12b3
pouya350 <pravyparen1@mail.ru>
2026-05-21 00:56:42 +0300

    crossref splited

---
+++ polozhuk.erofey/S4/crossref.cpp
@@ -0,0 +1,70 @@
+#include "crossref.hpp"
+
+std::string polozhuk::CrossReference::clean_word(std::string word)
+{
+    std::string temp = "";
+    std::copy_if(word.cbegin(), word.cend(), std::back_inserter(temp),
+        [](char c) {
+            if (std::isalpha(c)) {
+                return true;
+            }
+            return false;
+        });
+    std::transform(temp.begin(), temp.end(), temp.begin(),
+        [](char c) {
+        return std::tolower(c);
+    });
+    return temp;
+}
+
+void polozhuk::CrossReference::search_word(const std::string& text)
+{
+    auto my_text = my_map_.find(clean_word(text));
+    if (my_text != my_map_.end()) {
+        std::copy(my_text->second.cbegin(), my_text->second.cend(),
+            std::ostream_iterator<size_t>(std::cout, ", "));
+        std::cout << std::endl;
+        return;
+    }
+    std::cout << "nothing found"<< std::endl;
+    return;
+}
+
+
+void polozhuk::CrossReference::print_index() const
+{
+    if (my_map_.empty()) {
+        std::cout << "empty map" << std::endl;
+        return;
+    }
+    std::for_each(my_map_.cbegin(), my_map_.cend(),
+        [](const std::pair<const std::string, std::vector<size_t>> &item) {
+        std::cout << item.first << " -> strings: ";
+        std::copy(item.second.cbegin(),item.second.cend(),
+                  std::ostream_iterator<size_t>(std::cout, ", ")
+        );
+        std::cout << std::endl;
+    });
+}
+
+void polozhuk::CrossReference::build_index(const std::string& filepath) {
+    std::ifstream ifs(filepath);
+    std::string line;
+    if (!ifs.is_open()) {
+        std::cerr << "Error opening";
+        throw std::runtime_error("Error opening");
+    }
+    size_t index = 0;
+    while (std::getline(ifs, line)) {
+        ++index;
+        std::istringstream iss(line);
+        using istrit = std::istream_iterator<std::string>;
+        std::for_each(istrit(iss), istrit(),
+        [this, index](const std::string& s) {
+            std::string cword = clean_word(s);
+            if (!cword.empty()) {
+                my_map_[cword].push_back(index);
+            }
+        });
+    }
+}
--- polozhuk.erofey/S4/crossref.hpp
+++ polozhuk.erofey/S4/crossref.hpp
@@ -12,57 +12,10 @@
 
 namespace polozhuk {
     class CrossReference {
-
     public:
-        void build_index(const std::string& filepath)
-        {
-            std::ifstream ifs(filepath);
-            std::string line;
-            if (!ifs.is_open()) {
-                std::cerr << "Error opening";
-                throw std::runtime_error("Error opening");
-            }
-            size_t index = 0;
-            while (std::getline(ifs, line)) {
-                ++index;
-                std::istringstream iss(line);
-                using istrit = std::istream_iterator<std::string>;
-                std::for_each(istrit(iss), istrit(),
-                [this, index](const std::string& s) {
-                    std::string cword = clean_word(s);
-                    if (!cword.empty()) {
-                        my_map_[cword].push_back(index);
-                    }
-                });
-            }
-        }
-
-        void print_index() const {
-            if (my_map_.empty()) {
-                std::cout << "empty map" << std::endl;
-                return;
-            }
-            std::for_each(my_map_.cbegin(), my_map_.cend(),
-                [](const std::pair<const std::string, std::vector<size_t>> &item) {
-                std::cout << item.first << " -> strings: ";
-                std::copy(item.second.cbegin(),item.second.cend(),
-                          std::ostream_iterator<size_t>(std::cout, ", ")
-                );
-                std::cout << std::endl;
-            });
-        }
-        void search_word(const std::string& text) {
-            auto my_text = my_map_.find(clean_word(text));
-            if (my_text != my_map_.end()) {
-                std::copy(my_text->second.cbegin(), my_text->second.cend(),
-                    std::ostream_iterator<size_t>(std::cout, ", "));
-                std::cout << std::endl;
-                return;
-            }
-            std::cout << "nothing found"<< std::endl;
-            return;
-        }
-
+        void build_index(const std::string& filepath);
+        void print_index() const;
+        void search_word(const std::string& text);
         void clear_index() {
             my_map_.clear();
         }
@@ -71,21 +24,7 @@ namespace polozhuk {
         using iterator = map_::iterator;
         using const_iterator = map_::const_iterator;
         map_ my_map_;
-        std::string clean_word(std::string word) {
-            std::string temp = "";
-            std::copy_if(word.cbegin(), word.cend(), std::back_inserter(temp),
-                [](char c) {
-                    if (std::isalpha(c)) {
-                        return true;
-                    }
-                    return false;
-                });
-            std::transform(temp.begin(), temp.end(), temp.begin(),
-                [](char c) {
-                return std::tolower(c);
-            });
-            return temp;
-        }
+        static std::string clean_word(std::string word);
     };
 };
 #endif
