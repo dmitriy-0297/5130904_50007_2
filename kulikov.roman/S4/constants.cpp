6bf0189806e2ad5a56f412fd20cb8fad0875be2a
MARO <kulikovrma@yandex.ru>
2026-05-23 22:46:57 +0300

    Create constants.cpp

---
+++ kulikov.roman/S4/constants.cpp
@@ -0,0 +1,5 @@
+#include "constants.h"
+
+const char* const EMPTY_WORD_ERR    = "Word cannot be empty";
+const char* const WORD_NOT_FOUND_ERR = "Word not found";
+const char* const DICT_EMPTY_ERR    = "Dictionary is empty";
