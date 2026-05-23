2307752ac7d7b13476685e2f7227d9d14018ce79
MARO <kulikovrma@yandex.ru>
2026-05-23 22:46:40 +0300

    Create constants.h

---
+++ kulikov.roman/S4/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const char* const EMPTY_WORD_ERR;
+extern const char* const WORD_NOT_FOUND_ERR;
+extern const char* const DICT_EMPTY_ERR;
+
+#endif
